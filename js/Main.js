27d6599b01b35845623d956233081f7216778af8
oritvladi <oritvladi@gmail.com>
2024-10-29 16:25:13 +0200

    Deleted Main.js

--- js/Main.js
+++
@@ -1,6 +0,0 @@
-// function highlightImage(imageId) {
-//   var image = document.getElementById(imageId);
-//   image.style.width = image.clientWidth + 10 + "px";
-// }
-// window.onload=function mess(){alert('התחברת בהצלחה!');}
-
